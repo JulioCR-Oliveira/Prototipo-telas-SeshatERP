fd2324d2c74f1e92bcab3670e2a1e4ee1648272b
Júlio César R. de Oliveira <julioc.r.o@hotmail.com>
2022-12-04 18:14:37 -0300

    Alterações fichas

--- js/ficha.js
+++ js/ficha.js
@@ -1,9 +1,10 @@
 $("document").ready(function() {
 	toggleSearch();
+	$("#ficha-compra").toggle();
 	$("#cor-select").change(function() {redictTo($("#cor-select").val());});
 	$("#buscar-ref").click(function() {toggleSearch();});
 	$("#search-ref-form-submit").click(function() {toggleSearch();});
-	
+	$(".tab").click(function () { changeTab($(this)); });
 });
 
 function redictTo(url) {
@@ -16,4 +17,13 @@ function redictTo(url) {
 function toggleSearch() {
 	$("#buscar-ref").toggle();
 	$("#search-ref-form").toggle();
+};
+
+function changeTab(tab) {
+	if (tab.hasClass("unselected-tab")) {
+		$(".selected-tab").removeClass("selected-tab").addClass("unselected-tab");	
+		tab.addClass("selected-tab").removeClass("unselected-tab");
+		$("#setores").toggle();
+		$("#ficha-compra").toggle();
+	}
 };
